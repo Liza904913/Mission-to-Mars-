7ec044daa339269870fd1fbd9c90f915462e1be4
Liza Lyadova <elizaveta339@gmail.com>
2021-10-23 20:12:48 -0400

    New file

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Mission-to-Mars-
+
+## Project Overview
+
+The purpose of the project is to create a web application with Flask that will automatically scrape different websites to extract data about the Mission to Mars, store in a NoSQL database and display it on a HTML page.
+
+## Results 
+
+![Image1.png](Image1.png)
+
+[Image2.png](Image2.png)
--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_pymongo import PyMongo
 import scraping
 
 app = Flask(__name__)
-
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
@@ -12,6 +11,7 @@ mongo = PyMongo(app)
 def index():
    mars = mongo.db.mars.find_one()
    return render_template("index.html", mars=mars)
+   
 
 @app.route("/scrape")
 def scrape():
@@ -21,5 +21,5 @@ def scrape():
    return redirect('/', code=302)
 
 if __name__ == "__main__":
-   app.run()
+   app.run(debug=True)
 
